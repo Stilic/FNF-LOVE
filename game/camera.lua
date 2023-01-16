3bbf1873f49a45d3c0a272519172189f051f7cfd
Stilic <lateman-jpeg@outlook.fr>
2023-01-16 07:20:41 +0100

    some more shit grrr

--- game/camera.lua
+++ game/camera.lua
@@ -1,31 +1,17 @@
 local Camera = Object:extend()
 
-Camera.transform = love.math.newTransform()
-Camera.currentCamera = nil
-
 function Camera:new(x, y)
     if x == nil then x = 0 end
     if y == nil then y = 0 end
-    rawset(self, "x", x)
-    rawset(self, "y", y)
+    self.x = x
+    self.y = y
     self.angle = 0
     self.zoom = 1
 end
 
-function Camera:updateTransform(force)
-    if force or
-        (Camera.currentCamera ~= self and (Camera.currentCamera == nil or
-            (Camera.currentCamera.x ~= self.x and Camera.currentCamera.y ~= y))) then
-        Camera.currentCamera = self
-        Camera.transform:reset()
-        Camera.transform:translate(push.getWidth() * 0.5, push.getHeight() * 0.5)
-        Camera.transform:translate(-self.x, -self.y)
-    end
-end
-
 function Camera:getPosition(x, y)
-    self:updateTransform()
-    return Camera.transform:transformPoint(x, y)
+    return x - self.x + push.getWidth() * 0.5,
+           y - self.y + push.getHeight() * 0.5
 end
 
 function Camera:getObjectPosition(obj)
@@ -38,23 +24,16 @@ function Camera:getObjectPosition(obj)
 end
 
 function Camera:attach()
-    self:updateTransform()
     love.graphics.push()
 
     local w2, h2 = push.getWidth() * 0.5, push.getHeight() * 0.5
     love.graphics.scale(self.zoom)
-    love.graphics.translate(w2, h2)
     love.graphics.translate(w2 / self.zoom - w2, h2 / self.zoom - h2)
+    love.graphics.translate(w2, h2)
     love.graphics.rotate(-self.angle)
     love.graphics.translate(-w2, -h2)
 end
 
 function Camera:detach() love.graphics.pop() end
-
-function Camera:__newindex(k, v)
-    local d = (k == "x" and v ~= self.x) or (k == "y" and v ~= self.y)
-    rawset(self, k, v)
-    if d then self:updateTransform(true) end
-end
 
 return Camera
