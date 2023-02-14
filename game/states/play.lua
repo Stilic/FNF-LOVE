ae5b0101597fadd091b9f894824abf60df5fc77d
Stilic <lateman-jpeg@outlook.fr>
2023-02-14 17:07:59 +0100

    d

--- game/states/play.lua
+++ game/states/play.lua
@@ -21,7 +21,7 @@ function PlayState.sortByShit(a, b) return a.time < b.time end
 function PlayState:enter()
 	self.keysPressed = {}
 
-	local song = "triple-trouble"
+	local song = "hedge"
 	local chart = paths.getJSON("songs/" .. song .. "/" .. song).song
 	PlayState.song = {
 		name = chart.name,
