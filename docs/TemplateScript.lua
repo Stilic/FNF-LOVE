914f4855da57fa24447cce65308b8324481393dc
Stilic <lateman-jpeg@outlook.fr>
2023-09-15 20:32:01 +0200

    uwu

--- docs/TemplateScript.lua
+++ docs/TemplateScript.lua
@@ -1,65 +1,41 @@
 --- gets called BEFORE registering all of the variables and creating objects, used for creating objects
-function create()
-    
-end
+function create() end
 
 --- gets called AFTER registering all of the variables and creating objects, used for setting values
-function postCreate()
-    
-end
+function postCreate() end
 
 --- gets called every frame
 ---@param elapsed integer
-function update(elapsed)
-
-end
+function update(elapsed) end
 
 --- gets called every frame AFTER update.super()
 ---@param elapsed integer
-function postUpdate(elapsed)
-
-end
+function postUpdate(elapsed) end
 
 --- gets called BEFORE drawing all off the sprites onto the screen
-function draw()
-
-end
+function draw() end
 
 --- gets called AFTER drawing all off the sprites onto the screen
-function postDraw()
-
-end
+function postDraw() end
 
 --- gets called every stepHit
 ---@param curStep integer
-function step(curStep)
-
-end
+function step(curStep) end
 
 --- gets called every stepHit AFTER beat.super()
 ---@param curStep integer
-function postStep(curStep)
-
-end
+function postStep(curStep) end
 
 --- gets called every beatHit
 ---@param curBeat integer
-function beat(curBeat)
-
-end
+function beat(curBeat) end
 
 --- gets called every beatHit AFTER beat.super()
 ---@param curBeat integer
-function postBeat(curBeat)
-
-end
+function postBeat(curBeat) end
 
 -- Called before destroying Inst/Vocals 
-function leave()
-
-end
+function leave() end
 
 --- Called after leaving state completely
-function postLeave()
-
-end
+function postLeave() end
