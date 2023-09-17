daaff1afa2faf923d68817a2f3ae4c5b62c57281
Vi <vk15@tuta.io>
2023-09-17 19:44:05 -0300

    hhhhjdjhddovslbhpskoxl:(

--- game/states/play.lua
+++ game/states/play.lua
@@ -212,8 +212,7 @@ function PlayState:enter()
     self.scoreTxt = Text(0, self.healthBarBG.y + 30, "",
                          paths.getFont("vcr.ttf", 16), {1, 1, 1}, "center")
     self.scoreTxt.outWidth = 1
-    self.scoreTxt:setContent("Score: 0 // Misses: 0 // ???%")
-    self.scoreTxt:screenCenter('x')
+    self:recalculateRating()
 
     self.judgeSprTimer = Timer.new()
 
