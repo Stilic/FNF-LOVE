d531d18a710a55612d287945e978ecefc7fe7cda
Stilic <lateman-jpeg@outlook.fr>
2023-09-30 01:56:50 +0200

    codename engine moment

--- game/gameplay/character.lua
+++ game/gameplay/character.lua
@@ -81,8 +81,7 @@ end
 function Character:beat(b)
     self.script:call("beat", b)
     if self.lastHit > 0 then
-        if self.lastHit +
-            math.round(PlayState.inst.stepCrochet * self.singDuration) <
+        if self.lastHit + PlayState.inst.stepCrochet * self.singDuration <
             PlayState.songPosition then
             self:dance()
             self.lastHit = 0
