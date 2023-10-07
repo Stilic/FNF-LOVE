f96d5d82e4c903758c2914bfd45af8994db875a9
Vi <vk15@tuta.io>
2023-10-07 20:18:54 -0300

    e h

--- game/input/mouse.lua
+++ game/input/mouse.lua
@@ -112,7 +112,7 @@ function Mouse.onReleased(button)
 end
 
 function Mouse.onMoved(x, y)
-    Mouse.x, Mouse.y = love.graphics.transformPoint(x, y)
+    Mouse.x, Mouse.y = util.remapToGame(x, y)
     Mouse.screenX, Mouse.screenY = x, y
     Mouse.isMoved = true
 end
