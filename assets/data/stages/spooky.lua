c32d63ae2ea30bc60fc730093e44f328dbd7ef45
Vi <vk15@tuta.io>
2023-11-05 21:18:43 -0300

    silly (beta mix)

--- assets/data/stages/spooky.lua
+++ assets/data/stages/spooky.lua
@@ -12,8 +12,8 @@ end
 
 local lightningStrikeBeat = 0
 local lightningOffset = love.math.random(8, 24)
-function beat(b)
-    if love.math.randomBool(10) and b > lightningStrikeBeat + lightningOffset then
+function beat()
+    if love.math.randomBool(10) and curBeat > lightningStrikeBeat + lightningOffset then
         game.sound.play(paths.getSound('gameplay/thunder_' ..
                                            love.math.random(1, 2)))
         hallowBG:play('lightning')
