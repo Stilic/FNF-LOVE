1d67fc416e971cc87726732a038587e172743e50
Stilic <lateman-jpeg@outlook.fr>
2023-11-08 10:51:25 +0100

    Add files via upload

--- funkin/states/editors/charting.lua
+++ funkin/states/editors/charting.lua
@@ -443,7 +443,7 @@ function ChartingState:update(dt)
 end
 
 function ChartingState:loadSong(song)
-    game.sound.loadMusic(paths.getInst(songName))
+    game.sound.loadMusic(paths.getInst(songName), nil, false)
     ChartingState.conductor = Conductor(game.sound.music, self.__song.bpm)
     ChartingState.conductor.sound:setLooping(true)
     if self.__song.needsVoices then
