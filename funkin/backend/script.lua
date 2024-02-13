5392e25c61ca1e0ba58f80767bb3956e63b274ef
Vi <vk15@tuta.io>
2024-02-13 10:32:38 -0300

    feat: execute scripts safely

--- funkin/backend/script.lua
+++ funkin/backend/script.lua
@@ -12,26 +12,33 @@ function Script:new(path, notFoundMsg)
 	self.notFoundMsg = (notFoundMsg == nil and true or false)
 	self.closed = false
 
-	local p = path
+	local s, err = pcall(function()
+		local p = path
 
-	local chunk = paths.getLua(p)
-	if chunk then
-		setfenv(chunk, setmetatable(self.variables, chunkMt))
-		chunk()
-	else
-		if not self.notFoundMsg then return end
-		print("script not found for " .. paths.getPath(p))
+		local chunk = paths.getLua(p)
+		if chunk then
+			setfenv(chunk, setmetatable(self.variables, chunkMt))
+			chunk()
+		else
+			if not self.notFoundMsg then return end
+			print("script not found for " .. paths.getPath(p))
+			self.closed = true
+			return
+		end
+
+		p = path
+		if not p:endsWith("/") then p = p .. "/" end
+		self:set('Event_Continue', Script.Event_Continue)
+		self:set('Event_Cancel', Script.Event_Cancel)
+		self:set("SCRIPT_PATH", p)
+		self:set("close", function() self:close() end)
+		self:set("state", game.getState())
+	end)
+
+	if not s then
+		print('oh no script is returning an error NOOOO: ' .. err)
 		self.closed = true
-		return
 	end
-
-	p = path
-	if not p:endsWith("/") then p = p .. "/" end
-	self:set('Event_Continue', Script.Event_Continue)
-	self:set('Event_Cancel', Script.Event_Cancel)
-	self:set("SCRIPT_PATH", p)
-	self:set("close", function() self:close() end)
-	self:set("state", game.getState())
 end
 
 function Script:set(var, value)
@@ -44,9 +51,17 @@ function Script:call(func, ...)
 	if self.closed then return true end
 	local f = self.variables[func]
 	if f and type(f) == "function" then
-		local r = f(...)
-		if r ~= nil and pcall(type, r) then return r end
-		return true
+		local s, err = pcall(f, ...)
+		if s then
+			if err ~= nil and pcall(type, err) then
+				return err
+			end
+			return true
+		else
+			print('oh no script is returning an error at ' ..
+				func .. " NOOOO: " .. err)
+			self.closed = true
+		end
 	end
 	return nil
 end
