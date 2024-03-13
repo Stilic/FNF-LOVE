749ef422a4b8de9c3226c5425988e5fa5fd744bf
Vi <vk15@tuta.io>
2024-03-13 19:15:42 -0300

    uuh

--- loxel/util/color.lua
+++ loxel/util/color.lua
@@ -31,9 +31,44 @@ function Color.HSL(h, s, l)
 	return r + m, g + m, b + m
 end
 
-function Color.HSLtoRGB(...)
-	local r, g, b = Color.HSL(...)
-	return r * 255, g * 255, b * 255
+function Color.HSLtoRGB(h, s, l)
+	local C = ( 1 - math.abs( l + l - 1 ))*s
+	local m = l - 0.5*C
+	local r, g, b = m, m, m
+	if h == h then
+		local h_ = (h % 1.0) * 6.0
+		local X = C * (1 - math.abs(h_ % 2 - 1))
+		C, X = C + m, X + m
+		if     h_ < 1 then r, g, b = C, X, m
+		elseif h_ < 2 then r, g, b = X, C, m
+		elseif h_ < 3 then r, g, b = m, C, X
+		elseif h_ < 4 then r, g, b = m, X, C
+		elseif h_ < 5 then r, g, b = X, m, C
+		else               r, g, b = C, m, X
+		end
+	end
+	return r, g, b
+end
+
+function Color.RGBtoHSL(r, g, b)
+	local max = math.max(r, g, b)
+	local min = math.min(r, g, b)
+	local h, s, l = 0, 0, (max + min) / 2
+
+	if max ~= min then
+		local d = max - min
+		s = l > 0.5 and d / (2 - max - min) or d / (max + min)
+		if max == r then
+			h = (g - b) / d + (g < b and 6 or 0)
+		elseif max == g then
+			h = (b - r) / d + 2
+		else
+			h = (r - g) / d + 4
+		end
+		h = h / 6
+	end
+
+	return h, s, l
 end
 
 function Color.fromHSL(...)
