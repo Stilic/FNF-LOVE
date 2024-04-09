4a328228257b936377585677ae699e8c25c267f4
Ralty <78720179+Raltyro@users.noreply.github.com>
2024-04-10 05:34:02 +0700

    ok maybe lower the minimum height tamper of sustai

--- funkin/gameplay/notefield.lua
+++ funkin/gameplay/notefield.lua
@@ -2,8 +2,9 @@ local Notefield = ActorGroup:extend("Notefield")
 
 local safeZoneOffset = 0.189
 Notefield.ratings = {
-	{name = "perfect",	time = 0.015,			score = 350, splash = true,  mod = 1.2},
+	{name = "perfect",	time = 0.018,			score = 350, splash = true,  mod = 1.2},
 	{name = "sick",		time = 0.045,			score = 350, splash = true,  mod = 1.0},
+	{name = "nice",		time = 0.062,			score = 200, splash = false, mod = 0.85},
 	{name = "good",		time = 0.094,			score = 200, splash = false, mod = 0.7},
 	{name = "bad",		time = 0.140,			score = 100, splash = false, mod = 0.4},
 	{name = "shit",		time = safeZoneOffset,	score = 50,  splash = false, mod = 0.2}
