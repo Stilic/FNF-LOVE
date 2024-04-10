33d76a089a7fd4a59751b2930b03273791f7fa67
Vi <135684633+VicKaoy@users.noreply.github.com>
2024-04-10 19:04:41 -0300

    Remove `Color.HSL`, `Color.HSLtoRGB` does the same thing

--- loxel/util/color.lua
+++ loxel/util/color.lua
@@ -13,28 +13,6 @@ local colorTable = {
 ---@class Color:ColorTable
 local Color = {}
 
-function Color.HSL(h, s, l)
-	if s <= 0 then return l, l, l end
-	h = (h / 360) * 6
-	local c = (1 - math.abs(2 * l - 1)) * s
-	local x = (1 - math.abs(h % 2 - 1)) * c
-	local m, r, g, b = (l - .5 * c), 0, 0, 0
-	if h < 1 then
-		r, g, b = c, x, 0
-	elseif h < 2 then
-		r, g, b = x, c, 0
-	elseif h < 3 then
-		r, g, b = 0, c, x
-	elseif h < 4 then
-		r, g, b = 0, x, c
-	elseif h < 5 then
-		r, g, b = x, 0, c
-	else
-		r, g, b = c, 0, x
-	end
-	return r + m, g + m, b + m
-end
-
 function Color.HSLtoRGB(h, s, l)
 	local c = (1 - math.abs(l + l - 1)) * s
 	local m = l - 0.5 * c
