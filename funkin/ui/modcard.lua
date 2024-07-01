22427192c1d56634ed1af3b6395bc2b069b99cea
Vi <vk15@tuta.io>
2024-06-30 21:36:44 -0300

    modcard improvements

--- funkin/ui/modcard.lua
+++ funkin/ui/modcard.lua
@@ -5,38 +5,49 @@ function ModCard:new(x, y, mods)
 
 	local metadata = Mods.getMetadata(mods)
 
-	self.bg = Graphic(0, 0, 420, 620, Color.BLACK)
+	local enabledColor = (Mods.currentMod == mods and Color.GREEN or Color.RED)
+	self.enableCheck = Graphic(-3, -3, 438, 606, enabledColor, nil, "line")
+	self.enableCheck.line.width = 3
+	self.enableCheck.config.round = {16, 16}
+	self:add(self.enableCheck)
+
+	self.bg = Graphic(0, 0, 432, 600, Color.BLACK)
 	self.bg.alpha = 0.5
-	self.bg.config.round = {8, 8}
+	self.bg.config.round = {16, 16}
 	self:add(self.bg)
 
-	self.banner = Sprite(4, 4):loadTexture(Mods.getBanner(mods))
-	self.banner:setGraphicSize(412)
+	self.banner = Sprite(0, 8):loadTexture(Mods.getBanner(mods))
+	self.banner:setGraphicSize(self.bg.width - 16)
 	self.banner:updateHitbox()
+	self.banner.x = (self.bg.width - self.banner.width) / 2
 	self:add(self.banner)
 
-	self.titleBG = Graphic(4, 134, 412, 80, Color.BLACK)
+	self.titleBG = Graphic(0, 8, 0, 0, Color.BLACK)
+	self.titleBG.width, self.titleBG.height = self.bg.width - 16, 40
+	self.titleBG.x, self.titleBG.y = (self.bg.width - self.titleBG.width) / 2,
+		self.titleBG.y + self.banner.height + 8
 	self.titleBG.alpha = 0.3
-	self.titleBG.config.round = {8, 8}
+	self.titleBG.config.round = {16, 16}
 	self:add(self.titleBG)
 
-	self.titleTxt = Text(6, 136, metadata.name, paths.getFont("phantommuff.ttf", 30),
-		Color.WHITE, "center", 410)
+	self.titleTxt = Text(0, 0, metadata.name, paths.getFont("phantommuff.ttf", 30),
+		Color.WHITE, "center", self.titleBG.width - 8)
+	self.titleBG.height = self.titleTxt:getHeight() + 8
+	self.titleTxt.x, self.titleTxt.y = self.titleBG.x + (self.bg.width - self.titleBG.width) / 2, self.titleBG.y +
+		(self.titleBG.height - self.titleTxt:getHeight()) / 2
 	self:add(self.titleTxt)
 
-	self.descBG = Graphic(4, 218, 412, 340, Color.BLACK)
+	self.descBG = Graphic(0, 0, 0, 0, Color.BLACK)
+	self.descBG.width, self.descBG.height = self.bg.width - 16, self.bg.height - (
+		self.titleBG.y + self.titleBG.height) - 16
+	self.descBG.x, self.descBG.y = (self.bg.width - self.descBG.width) / 2, self.titleBG.y + self.titleBG.height + 8
 	self.descBG.alpha = 0.5
-	self.descBG.config.round = {8, 8}
+	self.descBG.config.round = {16, 16}
 	self:add(self.descBG)
 
-	self.descTxt = Text(14, 224, metadata.description, love.graphics.newFont(13),
-		Color.WHITE, "left", 392)
+	self.descTxt = Text(self.descBG.x + 8, self.descBG.y + 8, metadata.description, love.graphics.newFont(16),
+		Color.WHITE, "left", descBG.width - 16)
 	self:add(self.descTxt)
-
-	local enabledColor = (Mods.currentMod == mods and Color.GREEN or Color.RED)
-	self.enableCheck = Graphic(4, 562, 15, 54, enabledColor)
-	self.enableCheck.config.round = {6, 6}
-	self:add(self.enableCheck)
 end
 
 return ModCard
