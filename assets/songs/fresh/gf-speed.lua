248c746dd515488474ee5ef9ca91a6de434ee7c5
Stilic <lateman-jpeg@outlook.fr>
2024-07-02 02:34:15 +0200

    guh

--- assets/songs/fresh/gf-speed.lua
+++
@@ -1,8 +0,0 @@
-function beat()
-	switch(curBeat, {
-		[16] = function() state.gf.danceSpeed = 2 end,
-		[48] = function() state.gf.danceSpeed = 1 end,
-		[80] = function() state.gf.danceSpeed = 2 end,
-		[112] = function() state.gf.danceSpeed = 1 end
-	})
-end
