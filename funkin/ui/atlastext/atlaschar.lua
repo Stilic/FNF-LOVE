07196bbcb36e5a92e815da852621e4bc7a082980
Vi <vk15@tuta.io>
2024-07-18 21:38:20 -0300

    `AtlasText` automatic batching and more improves

--- funkin/ui/atlastext/atlaschar.lua
+++ funkin/ui/atlastext/atlaschar.lua
@@ -1,18 +1,18 @@
 local AtlasChar = Sprite:extend("AtlasChar")
 
-function AtlasChar:new(x, y, font, char)
+function AtlasChar:new(x, y, font, imageData, char)
 	AtlasChar.super.new(self, x, y)
 
 	self.font = font or "default"
 	self.char = char or "#"
-	self:setFont()
-	self:setChar()
+	if font then self:setFont(nil, imageData) end
+	if char then self:setChar() end
 end
 
-function AtlasChar:setFont(font)
+function AtlasChar:setFont(font, frames)
 	if font and self.font == font then return end
 	self.font = font or self.font
-	self:setFrames(paths.getSparrowAtlas('fonts/' .. self.font.name))
+	self:setFrames(frames)
 
 	self:updateHitbox()
 	self:setGraphicSize(self.width * self.font.scale)
@@ -51,6 +51,40 @@ function AtlasChar:setChar(letter)
 	local scale = self.font.scale
 	local x, y = ox * scale, (oy - (110 - self.height)) * scale
 	self.offset = {x = x, y = y}
+end
+
+function AtlasChar:__render()
+	return -- do not render anything, this will be done at __batch
+end
+
+function AtlasChar:__batch(batch, camera)
+	batch:setColor(Color.vec4(self.color, self.alpha))
+
+	local f = self:getCurrentFrame()
+	local x, y, rad, sx, sy, ox, oy = self.x, self.y, math.rad(self.angle),
+		self.scale.x * self.zoom.x, self.scale.y * self.zoom.y,
+		self.origin.x, self.origin.y
+
+	if self.flipX then sx = -sx end
+	if self.flipY then sy = -sy end
+
+	x, y = x + ox - self.offset.x - (camera.scroll.x * self.scrollFactor.x),
+		y + oy - self.offset.y - (camera.scroll.y * self.scrollFactor.y)
+
+	if f then
+		ox, oy = ox + f.offset.x, oy + f.offset.y
+		if not self._batchIndex then
+			self._batchIndex = batch:add(f.quad, x, y, rad, sx, sy, ox, oy)
+		else
+			batch:set(self._batchIndex, f.quad, x, y, rad, sx, sy, ox, oy)
+		end
+	else
+		if not self._batchIndex then
+			self._batchIndex = batch:add(x, y, rad, sx, sy, ox, oy)
+		else
+			batch:set(self._batchIndex, x, y, rad, sx, sy, ox, oy)
+		end
+	end
 end
 
 return AtlasChar
--- funkin/ui/atlastext/atlastext.lua
+++ funkin/ui/atlastext/atlastext.lua
@@ -1,22 +1,15 @@
 local AtlasText = SpriteGroup:extend("AtlasText")
 local AtlasChar = require "funkin.ui.atlastext.atlaschar"
 
+AtlasText.defaultFont = paths.getJSON("data/fonts/default")
 AtlasText.tabSize = 2
-AtlasText.lineSize = 80
-AtlasText.spaceWidth = 50
-
-function AtlasText.getCharWidth(char, fnt)
-	local obj = AtlasChar(0, 0, fnt, char)
-	local width = obj.width
-	obj:destroy()
-	return width
-end
+AtlasText.lineSize = 86
+AtlasText.spaceWidth = 28
 
 function AtlasText.getFont(font, size)
 	font = font or "default"
-
-	local exists = paths.exists(paths.getPath("data/fonts/" .. font .. ".json"), "file")
-	local font = paths.getJSON("data/fonts/" .. (exists and font or "default"))
+	font = paths.getJSON("data/fonts/" .. font)
+	if font == nil then font = AtlasText.defaultFont end
 	if size ~= nil then font.scale = size end
 	font.scale = font.scale or 1
 	font.lineSize = font.lineSize or AtlasText.lineSize
@@ -30,76 +23,77 @@ function AtlasText:new(x, y, text, font, limit, align)
 	self.text = text or ""
 	self.limit = limit or 0
 	self.align = align or "left"
-	-- Align can be left, right, or center
-
-	self.font = nil
-	self:setFont(font)
-	self:setTyping(0)
 
 	self.__lines = {}
+	self.__batch = love.graphics.newSpriteBatch(Sprite.defaultTexture)
 
-	self:setText()
-	return self
+	self:setTyping(0)
+	self:setFont(font)
 end
 
 function AtlasText:setTyping(speed, sound)
-	self.typed = (speed > 0)
-
-	if not self.typed then return end
-	self.target, self.text = self.text, ""
-	self.speed, self.timer, self.index = 0.04, 0, 0
-	self.sound, self.completeCallback = sound, nil
-	self.finished = not self.typed
-
-	self:setText()
+	self.typed = speed > 0
+	self.__target, self.timer, self.index = self.text, 0, 0
+	self.sound, self.speed, self.completeCallback = sound, speed, nil
+	if self.typed then self.text = "" end
 end
 
 function AtlasText:setFont(font, size)
 	if (font and self.font == font) and (size and self.size == size) then
 		return
 	end
-
 	self.font = type(font) == "string" and
-		AtlasText.getFont(font) or font
-	self:setText(self.text)
+		AtlasText.getFont(font, size) or font
+
+	self.imageData = paths.getSparrowAtlas('fonts/' .. self.font.name)
+	self.__batch:setTexture(self.imageData.texture)
+
+	self:setText()
+end
+
+function AtlasText:getCharWidth(char)
+	if char == " " or char == "	" then
+		return (self.font.spaceWidth * (char == "	" and
+			AtlasText.tabSize or 1)) * self.font.scale
+	elseif char ~= "\n" then
+		local obj = AtlasChar(0, 0, self.font, self.imageData, char)
+		local w = obj.width; obj:destroy()
+		return w
+	end
+	return 0
 end
 
 function AtlasText:setText(text)
 	self:__clear()
+	if text ~= nil then self.text = text end
 
-	if text then self.text = text end
-
-	local curLine = ""
-	local width = 0
-
-	for i = 1, #self.text do
-		local char = self.text:sub(i, i)
-		if char == " " then
-			curLine = curLine .. char
-			width = width + self.font.spaceWidth * self.font.scale
-		elseif char == "	" then -- tab
-			curLine = curLine .. char
-			width = width + (self.font.spaceWidth * AtlasText.tabSize) * self.font.scale
-		elseif char == "\n" or (self.limit > 0 and width >= self.limit) then
-			table.insert(self.__lines, {text = curLine, width = width})
-			curLine = (char == "\n" or char == " ") and "" or char
-			width = char == "\n" and 0 or AtlasText.getCharWidth(char, self.font)
+	local line, width = "", 0
+	for _, char in ipairs(self.text:split()) do
+		local cWidth = self:getCharWidth(char)
+		if char == "\n" or (self.limit > 0 and width + cWidth >= self.limit) then
+			table.insert(self.__lines, {t = line, w = width})
+			line = char == "\n" and "" or char
+			width = char == "\n" and 0 or cWidth
 		else
-			local letterWidth = AtlasText.getCharWidth(char, self.font)
-			curLine = curLine .. char
-			width = width + letterWidth
+			line = line .. char
+			width = width + cWidth
 		end
 	end
 
-	if #curLine > 0 then
-		table.insert(self.__lines, {text = curLine, width = width})
-	end
+	if #line > 0 then table.insert(self.__lines, {t = line, w = width}) end
 
-	if not self.__lines then return end
-	local y = 0
-	for _, line in ipairs(self.__lines) do
-		self:__createLine(line.text, y, line.width, self.align, self.limit)
-		y = y + self.font.lineSize
+	for i, curLine in ipairs(self.__lines) do
+		local x, xOff = 0, 0
+		if self.align ~= "left" then
+			xOff = (self.limit - curLine.w) / (self.align == "center" and 2 or 1)
+		end
+		local y = (i - 1) * (self.font.lineSize * self.font.scale)
+
+		for _, char in ipairs(curLine.t:split()) do
+			local letter = self:__makeChar(x + xOff, y, char)
+			x = x + letter.width
+			if letter.is then self:add(letter) end
+		end
 	end
 
 	self:updateHitbox()
@@ -112,7 +106,7 @@ function AtlasText:update(dt)
 			self:addLetter()
 		end
 
-		if self.index == #self.target then
+		if self.index == #self.__target then
 			self.finished = true
 			if self.completeCallback then self.completeCallback() end
 		end
@@ -124,55 +118,77 @@ end
 function AtlasText:forceEnd()
 	if not self.typed then return end
 
-	self.text = self.target
+	self.text = self.__target
 	self.finished = true
 	if self.completeCallback then self.completeCallback() end
-end
-
-function AtlasText:addLetter()
-	if not self.typed then return end
-
-	self.timer = 0
-	self.index = self.index + 1
-
-	self.text = string.sub(self.target, 1, self.index)
-	if self.sound then game.sound.play(self.sound) end
-
 	self:setText()
 end
 
-function AtlasText:__createLine(textL, y, lineWidth)
-	local x, xOff = 0, 0
+function AtlasText:addLetter()
+	self.timer = 0
+	self.index = self.index + 1
+	self.text = self.__target:sub(1, self.index)
+	if self.sound then game.sound.play(self.sound) end
+	self:setText()
+end
 
-	if self.align == "center" then
-		xOff = (self.limit - lineWidth) / 2
-	elseif self.align == "right" then
-		xOff = self.limit - lineWidth
+function AtlasText:__makeChar(x, y, char)
+	if char ~= "\n" and char ~= " " and char ~= "	" then
+		return AtlasChar(x, y, self.font, self.imageData, char)
 	end
+	return {width = self:getCharWidth(char)}
+end
 
-	for i = 1, #textL do
-		local char = textL:sub(i, i)
-		if char == " " then
-			x = x + self.font.spaceWidth * self.font.scale
-		elseif char == "	" then -- tab
-			x = x + (self.font.spaceWidth * AtlasText.tabSize) * self.font.scale
-		elseif char ~= "\n" then
-			local letter = AtlasChar(x + xOff, y, self.font, char)
-			x = x + letter.width
-			self:add(letter)
-		end
+function AtlasText:__render(camera)
+	local list = self.__cameraRenderQueue[camera]
+	if not list then return end
+
+	for i, member in ipairs(list) do
+		member:__batch(self.__batch, camera)
+		list[i] = nil
 	end
+	self.__cameraRenderQueue[camera] = nil
+	table.insert(self.__unusedCameraRenderQueue, list)
+
+	local oldState = Object.saveDrawState()
+	local min, mag, anisotropy = self.__batch:getTexture():getFilter()
+	local mode = self.antialiasing and "linear" or "nearest"
+	self.__batch:getTexture():setFilter(mode, mode, anisotropy)
+
+	local x, y, rad, sx, sy, ox, oy = self.x, self.y, math.rad(self.angle),
+		self.scale.x * self.zoom.x, self.scale.y * self.zoom.y,
+		self.origin.x, self.origin.y
+
+	if self.flipX then sx = -sx end
+	if self.flipY then sy = -sy end
+
+	x, y = x + ox - self.offset.x - (camera.scroll.x * self.scrollFactor.x),
+		y + oy - self.offset.y - (camera.scroll.y * self.scrollFactor.y)
+
+	love.graphics.setColor(Color.vec4(self.color, self.alpha))
+	love.graphics.setBlendMode(self.blend)
+	love.graphics.setShader(self.shader)
+
+	love.graphics.draw(self.__batch, x, y, rad, sx, sy, ox, oy)
+
+	self.__batch:getTexture():setFilter(min, mag, anisotropy)
+	Object.loadDrawState(oldState)
 end
 
 function AtlasText:__clear()
-	if not self.members then return end
-
 	for i = #self.members, 1, -1 do
 		local char = self.members[i]
 		char:destroy()
 		self:remove(char)
 	end
+	if self.__batch then self.__batch:clear() end
 	self.__lines = {}
+end
+
+function AtlasText:destroy()
+	self:__clear()
+	if self.__batch then self.__batch:release() end
+	AtlasText.super.destroy(self)
 end
 
 return AtlasText
