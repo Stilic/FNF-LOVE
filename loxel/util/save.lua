96bda458b4d00640f79a0803416afee8d3312290
Stilic <lateman-jpeg@outlook.fr>
2024-09-14 00:42:14 +0200

    fix saves

--- loxel/util/save.lua
+++ loxel/util/save.lua
@@ -2,7 +2,7 @@ local json = require("lib.json")
 
 local Save = {
 	data = {},
-	path = '',
+	path = "",
 	initialized = false
 }
 
@@ -10,44 +10,15 @@ function Save.init(name)
 	if Save.initialized then return end
 	Save.initialized = true
 
-	if love.system.getDevice() == "Mobile" then
-		Save.path = love.filesystem.getSaveDirectory()
-		local dataFile = love.filesystem.read(name .. '.lox')
-		if dataFile then
-			local decodeData = love.data.decode("string", "hex", dataFile)
-			Save.data = json.decode(decodeData)
-		end
-	else
-		Save.path = love.filesystem.getAppdataDirectory() .. '/' .. Project.company .. '/' .. Project.file
-		local filePath = Save.path .. '/' .. name .. '.lox'
-		local dataFile = io.open(filePath, "rb")
-		if dataFile then
-			local decodeData = love.data.decode("string", "hex", dataFile:read("a"))
-			Save.data = json.decode(decodeData)
-			dataFile:close()
-		end
+	Save.path = love.filesystem.getSaveDirectory()
+	local dataFile = love.filesystem.read(name .. ".lox")
+	if dataFile then
+		Save.data = json.decode(love.data.decode("string", "hex", dataFile))
 	end
 end
 
 function Save.bind(name)
-	local encodeData = love.data.encode("string", "hex", json.encode(Save.data))
-	if love.system.getDevice() == "Mobile" then
-		love.filesystem.write(name .. '.lox', encodeData)
-	else
-		local filePath = Save.path .. '/' .. name .. '.lox'
-		local dirCheck = io.open(filePath, "wb")
-		if not dirCheck then
-			local dirToMake = filePath:gsub('/' .. name .. '.lox', '')
-			if love.system.getOS() == "Windows" then
-				os.execute('mkdir "' .. dirToMake .. '"')
-			else
-				os.execute('mkdir -p "' .. dirToMake .. '"')
-			end
-		end
-		local saveFile = io.open(filePath, "wb")
-		saveFile:write(encodeData)
-		saveFile:close()
-	end
+	love.filesystem.write(name .. ".lox", love.data.encode("string", "hex", json.encode(Save.data)))
 end
 
 return Save
